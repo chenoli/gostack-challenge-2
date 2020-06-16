668a42a66f15fa1022d19e4c6565ad04da66a166
Carlos Henrique <oliveira.caco.h@gmail.com>
2020-06-15 22:04:42 -0300

    feat(repository): adds repository crud logic

--- src/__tests__/repositories.spec.js
+++ src/__tests__/repositories.spec.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
-const app = require("../app");
 const { isUuid } = require("uuidv4");
+
+const app = require("../app");
 
 describe("Repositories", () => {
   it("should be able to create a new repository", async () => {
